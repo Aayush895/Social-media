969d3f78e699cef5474f49e5f6e3898e9643bb9f
Aayush895 <aayush895@gmail.com>
2024-06-08 21:03:40 +0530

    Starter files part-2

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
 
 const app = express();
 app.use(express.json({ limit: "100kb" }));
+app.use(helmet());
+app.use(morgan("tiny"));
 
 // importing the created routes
+app.get("/", (req, res) => {
+  res.send("Welcome to the social media page");
+});
 
 export default app;
--- src/db/index.js
+++ src/db/index.js
@@ -3,12 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 const connectDb = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
-    );
-    console.log(
-      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
-    );
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(`\n MongoDB connected !!`);
   } catch (error) {
     console.log(error);
     process.exit(1);
